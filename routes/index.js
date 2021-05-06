977581a95d1638896179e7055536ad11339c7194
Aurelie75FR <aurelielo.vi@gmail.com>
2021-05-06 11:21:00 +0200

    iteration 6 done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+const users = [
+  { name: "Aurelie", email: "foo@aurelie", favoriteLangage: "JS" },
+  { name: "Quentin", email: "baz@quentin", favoriteLangage: "React.js" },
+  { name: "Alice", email: "bar@alice", favoriteLangage: "french" }
+];
+const images = ["anna.jpg", "sean.jpg", "team.jpg"]
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", {images});
 });
+
+router.get("/my-dev-squad", function (req, res, next) {
+  res.render("hackers", {users});
+});
+
 
 module.exports = router;
